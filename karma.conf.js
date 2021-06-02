9bce37682e12bf8f031377ed4478d6ecb996518b
Sean <shoonth@gmail.com>
2021-06-01 22:39:45 -0400

    test correct

--- karma.conf.js
+++ karma.conf.js
@@ -23,15 +23,18 @@ module.exports = function (config) {
         { type: 'text-summary' }
       ],
     },
-    ChromeHeadless:{
-      base: 'Chrome', flags: [ '--headless', '--disable-gpu',  '--remote-debugging-port=9222' ]
-    },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+          base: 'ChromeHeadless',
+          flags: ['--no-sandbox']
+      }
+    },
     singleRun: false,
     restartOnFileChange: true
   });
